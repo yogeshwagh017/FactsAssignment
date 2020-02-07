97f8f234eafc7ff8a1c8e98a1ca5b3ff934d486a
Kushal <kushal.choudhari@infosys.com>
2020-02-07 18:37:15 +0530

    Initial Commit

---
+++ FactsAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FactsAssignment
+//
+//  Created by kushal choudhari on 07/02/20.
+//  Copyright © 2020 Yogesh Wagh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
